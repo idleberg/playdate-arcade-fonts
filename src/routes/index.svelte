2144f12bad94e2bf6f466bfeef9fcd0638e40269
Jan T. Sott <git@idleberg.com>
2022-04-20 22:57:04 +0200

    fix footer

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,25 +1,27 @@
 <script language="typescript">
     import page from '../meta.json';
+
+    const publicFonts = page.content.filter(item => item.public);
 </script>
 
 <h1>{page.title}</h1>
 
-{#each page.content as font}
+{#each publicFonts as font, index}
     <section>
         <h1>{font.name}</h1>
 
-        <a href={`https://github.com/idleberg/playdate-arcade-fonts/blob/main/fonts/${encodeURIComponent(font.category)}/${encodeURIComponent(font.name)}.fnt`} target="_blank">
+        <a href={`/fonts/${encodeURIComponent(font.category)}/${encodeURIComponent(font.name)}.fnt`} download>
           <img
-            src={`https://raw.githubusercontent.com/idleberg/playdate-arcade-fonts/main/preview/${encodeURIComponent(font.category)}/${encodeURIComponent(font.name)}.png`}
+            src={`/previews/${font.category}/${font.name}.png`}
             alt={`Preview showing characters of the ${font.name} bitmap font.`}
             loading="lazy"
-            fetchpriority={font.index < 10 ? 'high' : 'auto'}
+            fetchpriority={index < 10 ? 'high' : 'auto'}
           />
         </a>
     </section>
 {/each}
 
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p><a href="https://github.com/idleberg/playdate-arcade-fonts">Code on GitHub</a></p>
 
 <style>
     img {
