db54a1fb253b67da75d18feb2028120850e98709
Jan T. Sott <git@idleberg.com>
2022-04-20 17:59:48 +0200

    update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -15,7 +17,7 @@ const config = {
 		})
 	},
 	paths: {
-		base: process.env.NODE_ENV === 'production' ? '/playdate-arcade-fonts' : '/'
+		base: dev ? '' : '/playdate-arcade-fonts'
 	}
 };
 
