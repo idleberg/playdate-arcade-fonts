f26c07382a1a7208b8b6dd1a0174d93317bb2f04
Jan T. Sott <git@idleberg.com>
2022-04-20 22:52:10 +0200

    restructure files, add wip font route

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,25 +1,25 @@
-<script language="typescript">
+<script lang="typescript">
     import page from '../meta.json';
 </script>
 
-<h1>{page.title}</h1>
-
-{#each page.content as font}
-    <section>
-        <h1>{font.name}</h1>
-
-        <a href={`https://github.com/idleberg/playdate-arcade-fonts/blob/main/fonts/${encodeURIComponent(font.category)}/${encodeURIComponent(font.name)}.fnt`} target="_blank">
-          <img
-            src={`https://raw.githubusercontent.com/idleberg/playdate-arcade-fonts/main/preview/${encodeURIComponent(font.category)}/${encodeURIComponent(font.name)}.png`}
-            alt={`Preview showing characters of the ${font.name} bitmap font.`}
-            loading="lazy"
-            fetchpriority={font.index < 10 ? 'high' : 'auto'}
-          />
-        </a>
-    </section>
-{/each}
-
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+    <h1>{page.title}</h1>
+    
+    {#each page.content as font, index}
+        <section>
+            <h1>{font.name}</h1>
+    
+            <a href={`/fonts/${font.name}`} {font}>
+              <img
+                src={`/preview/${font.category}/${font.name}.png`}
+                alt={`Preview showing characters of the ${font.name} bitmap font.`}
+                loading="lazy"
+                fetchpriority={index < 10 ? 'high' : 'auto'}
+              />
+            </a>
+        </section>
+    {/each}
+</main>
 
 <style>
     img {
