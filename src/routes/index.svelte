37041f5bcd6b97c5e87effdb40302df2d48cb399
Jan T. Sott <git@idleberg.com>
2022-04-22 21:36:47 +0200

    fix url

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,41 +1,91 @@
-<script lang="typescript">
-    import page from '../meta.json';
+<script lang="ts">
+    import store from '$lib/store';
+    import { onMount } from 'svelte';
+    import { Button, Column, Loading, Row, Tag, Search } from "carbon-components-svelte";
 
-    const publicFonts = sortByKey(page.content.filter(item => item.public), 'name');
+    let value: string = '';
+    let isLoading = true;
+    let publicFonts: Record<string, any>[] = [];
 
-    function sortByKey(arr) {
-        return arr.sort((a, b) =>  {
-            let x = a['name'];
-            let y = b['name'];
+    onMount(() => {
+        isLoading = false;
+    });
 
-            return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-        });
+    store.subscribe(value => {
+        publicFonts = value;
+    });
+
+    const changeHandler = () => {
+        if (value?.length) {
+            publicFonts = $store.filter(item => item.name.toLowerCase().includes(value.toLowerCase()) || item.category.toLowerCase().includes(value.toLowerCase()));
+        } else {
+            clearHandler();
+        }
+    }
+
+    const clearHandler = () => {
+        publicFonts = $store;
     }
 </script>
 
-<h1>{page.title}</h1>
+<Row>
+    <Column padding lg={6} md={4} sm={8}>
+        <Search placeholder="Search by name..." bind:value on:input={changeHandler} on:clear={clearHandler} />
+    </Column>
+</Row>
 
-{#each publicFonts as font, index}
-    <section>
-        <h1>{font.name}</h1>
+<Row class="high-row">
+    {#if publicFonts?.length}
+        {#each publicFonts as font, index}
+            <Column padding>
+                <Row>
+                    <Column>
+                        <div>
+                            <h2>{font.name}</h2>
+                            <Tag type="outline">{font.category}</Tag>
+                        </div>
+                            
+                        <!-- <a href={`/font/${font.name}`}> -->
+                        <img
+                            src={`${import.meta.env.VITE_HOMEPAGE}/previews/${font.category}/${font.name}.png`}
+                            alt={`Preview showing characters of the ${font.name} bitmap font.`}
+                            loading="lazy"
+                            fetchpriority={index < 10 ? 'high' : 'auto'}
+                        />
+                        <!-- </a> -->
+                    </Column>
+                </Row>
 
-        <a href={`${import.meta.env.VITE_HOMEPAGE}/fonts/${encodeURIComponent(font.category)}/${encodeURIComponent(font.name)}.fnt`} download>
-          <img
-            src={`${import.meta.env.VITE_HOMEPAGE}/previews/${font.category}/${font.name}.png`}
-            alt={`Preview showing characters of the ${font.name} bitmap font.`}
-            loading="lazy"
-            fetchpriority={index < 10 ? 'high' : 'auto'}
-          />
-        </a>
-    </section>
-{/each}
+                <Row>
+                    <Column padding>
+                        <!-- <Button href={`/font/${font.name}`} size="small" kind="ghost">Try</Button> -->
+                        <Button href={`${import.meta.env.VITE_HOMEPAGE}/fonts/${font.category}/${font.name}.fnt`} download={`${font.name}.fnt`} size="small" kind="ghost">Download</Button>
+                        <Button href={`https://github.com/idleberg/playdate-arcade-fonts/tree/main/static/fonts/${encodeURIComponent(font.category)}/${encodeURIComponent(font.name)}.fnt`} size="small" kind="ghost">Source</Button>
+                    </Column>
+                </Row>
+            </Column>
 
-<p><a href="https://github.com/idleberg/playdate-arcade-fonts">Code on GitHub</a></p>
+        {/each}
+    {:else}
+        <Column class="center" padding>
+            {#if isLoading}
+                <Loading withOverlay={false} small />
+            {:else}
+                No Results
+            {/if}
+        </Column>
+    {/if}
+</Row>
 
 <style>
     img {
-        width: auto;
-        height: 72px;
+        height: 108px;
         image-rendering: pixelated;
+        width: auto;
+    }
+
+    :global(.center) {
+        display: flex;
+        justify-content: center;
     }
 </style>
