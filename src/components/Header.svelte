682a804e8d70e31bef2399b8ce004f78713cfcbe
Jan T. Sott <git@idleberg.com>
2022-04-25 23:47:30 +0200

    use absolute url for home link

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,10 +1,11 @@
 <script>
      import { Column, Row } from "carbon-components-svelte";
+import { MeterAlt } from "carbon-icons-svelte";
 </script>
 
 <Row>
     <Column padding>
-        <a href="/"><h1>Playdate Arcade Fonts</h1></a>
+        <a href={import.meta.env.VITE_HOMEPAGE}><h1>Playdate Arcade Fonts</h1></a>
         <p>A collection of bitmap fonts for Playdate</p>
     </Column>
 </Row>
