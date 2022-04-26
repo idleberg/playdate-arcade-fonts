95a7e70e95c26944bca5f25cd13efc7d29eeeb00
Jan T. Sott <git@idleberg.com>
2022-04-26 08:43:12 +0200

    wrap content in condition

--- src/routes/details/[font].svelte
+++ src/routes/details/[font].svelte
@@ -4,7 +4,7 @@
     import page from '../../meta.json';
     
     // Components
-    import { Button, Column, Row, TextInput } from "carbon-components-svelte";
+    import { Button, Column, Loading, Row, TextInput } from "carbon-components-svelte";
     import Code from "carbon-icons-svelte/lib/Code.svelte";
     import Download from "carbon-icons-svelte/lib/Download.svelte";
     import Tags from '../../components/Tags.svelte';
@@ -44,83 +44,87 @@
 </script>
 
 <svelte:head>
-  <title>{`${font.name} | Playdate Arcade Fonts`}</title>
+  <title>{font?.name ? `${font.name} | Playdate Arcade Fonts` : 'Playdate Arcade Fonts'}</title>
 </svelte:head>
 
-<div style={`--backgroundImage: url("${import.meta.env.VITE_HOMEPAGE}/sprites/${encodeURIComponent(font.name)}.png")`}>
-    <!-- <Row>
-        <Column padding>
-            <Breadcrumb noTrailingSlash>
-                <BreadcrumbItem href="../">All Fonts</BreadcrumbItem>
-                <BreadcrumbItem href={`/category/${font.category}`}>{font.category}</BreadcrumbItem>
-                <BreadcrumbItem isCurrentPage>{font.name}</BreadcrumbItem>
-            </Breadcrumb>
-        </Column>
-    </Row> -->
-    
-    <Row>
-        <Column padding>
-            <Tags {font} />
-        </Column>
-    </Row>
+{#if font?.name}
+    <div style={font?.name && `--backgroundImage: url("${import.meta.env.VITE_HOMEPAGE}/sprites/${encodeURIComponent(font.name)}.png")`}>
+        <!-- <Row>
+            <Column padding>
+                <Breadcrumb noTrailingSlash>
+                    <BreadcrumbItem href="../">All Fonts</BreadcrumbItem>
+                    <BreadcrumbItem href={`/category/${font.category}`}>{font.category}</BreadcrumbItem>
+                    <BreadcrumbItem isCurrentPage>{font.name}</BreadcrumbItem>
+                </Breadcrumb>
+            </Column>
+        </Row> -->
+        
+        <Row>
+            <Column padding>
+                <Tags {font} />
+            </Column>
+        </Row>
 
-    <Row>
-        <Column padding md={16} lg={8}>
-            <TextInput
-                style={`max-height:400px`}
-                placeholder="Enter your text..."
-                labelText="Input"
-                on:input={handleChange}
-                value={defaultText}
-            />
-        </Column>
-    </Row>
+        <Row>
+            <Column padding md={16} lg={8}>
+                <TextInput
+                    style={`max-height:400px`}
+                    placeholder="Enter your text..."
+                    labelText="Input"
+                    on:input={handleChange}
+                    value={defaultText}
+                />
+            </Column>
+        </Row>
 
-    <Row>
-        <Column padding md={16} lg={8}>
-            <label for="preview-area" class="false bx--label">Output</label>
-            <ul id="preview-area">
-                <li bind:this={previewArea}></li>
-                <li class="zoom-2x" bind:this={previewArea2x}></li>
-                <li class="zoom-4x" bind:this={previewArea4x}></li>
-            </ul>
-            <small>The text above</small>
-        </Column>
-    </Row>
+        <Row>
+            <Column padding md={16} lg={8}>
+                <label for="preview-area" class="false bx--label">Output</label>
+                <ul id="preview-area">
+                    <li bind:this={previewArea}></li>
+                    <li class="zoom-2x" bind:this={previewArea2x}></li>
+                    <li class="zoom-4x" bind:this={previewArea4x}></li>
+                </ul>
+                <small>The text above</small>
+            </Column>
+        </Row>
 
-    <Row>
-        <Column padding>
-            {#if (font.features.variants)}
+        <Row>
+            <Column padding>
+                {#if (font.features.variants)}
+                    <Button
+                        href={`${import.meta.env.VITE_HOMEPAGE}/fonts/${font.category}/${font.name} (fixed width).fnt`}
+                        download={`${font.name} (fixed width).fnt`}
+                        icon={Download}
+                    >Fixed width</Button>
+                    <Button
+                        href={`${import.meta.env.VITE_HOMEPAGE}/fonts/${font.category}/${font.name} (auto-sized).fnt`}
+                        download={`${font.name} (auto-sized).fnt`}
+                        kind="tertiary"
+                        icon={Download}
+                    >Auto-sized</Button>
+                {:else}
+                    <Button
+                        href={`${import.meta.env.VITE_HOMEPAGE}/fonts/${font.category}/${font.name}.fnt`}
+                        download={`${font.name} (fixed width).fnt`}
+                        icon={Download}
+                    >Fixed width</Button>
+                    <Button
+                        icon={Download}
+                        disabled
+                    >Auto-sized</Button>
+                {/if}
                 <Button
-                    href={`${import.meta.env.VITE_HOMEPAGE}/fonts/${font.category}/${font.name} (fixed width).fnt`}
-                    download={`${font.name} (fixed width).fnt`}
-                    icon={Download}
-                >Fixed width</Button>
-                <Button
-                    href={`${import.meta.env.VITE_HOMEPAGE}/fonts/${font.category}/${font.name} (auto-sized).fnt`}
-                    download={`${font.name} (auto-sized).fnt`}
+                    href={`https://github.com/idleberg/playdate-arcade-fonts/tree/main/static/fonts/${encodeURIComponent(font.category)}/${encodeURIComponent(font.name)}.fnt`}
                     kind="tertiary"
-                    icon={Download}
-                >Auto-sized</Button>
-            {:else}
-                <Button
-                    href={`${import.meta.env.VITE_HOMEPAGE}/fonts/${font.category}/${font.name}.fnt`}
-                    download={`${font.name} (fixed width).fnt`}
-                    icon={Download}
-                >Fixed width</Button>
-                <Button
-                    icon={Download}
-                    disabled
-                >Auto-sized</Button>
-            {/if}
-            <Button
-                href={`https://github.com/idleberg/playdate-arcade-fonts/tree/main/static/fonts/${encodeURIComponent(font.category)}/${encodeURIComponent(font.name)}.fnt`}
-                kind="tertiary"
-                icon={Code}
-            >Source</Button>
-        </Column>
-    </Row>
-</div>
+                    icon={Code}
+                >Source</Button>
+            </Column>
+        </Row>
+    </div>
+{:else}
+    <Loading withOverlay={false} small />
+{/if}
 
 <style lang="scss">
     ul {
