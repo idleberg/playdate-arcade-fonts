ea4065ae2acc6d86fe6d83a79f6201266ea0690b
Jan T. Sott <git@idleberg.com>
2022-04-27 23:56:27 +0200

    change link order

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -9,10 +9,10 @@
                 <Link inline href="https://github.com/idleberg/playdate-arcade-fonts" target="_blank">GitHub</Link>
             </li>
             <li>
-                <Link inline href={`${import.meta.env.VITE_HOMEPAGE}/faq`}>FAQ</Link>
+                <Link inline href="https://creativecommons.org/publicdomain/zero/1.0/" target="_blank">License</Link>
             </li>
             <li>
-                <Link inline href="https://creativecommons.org/publicdomain/zero/1.0/" target="_blank">License</Link>
+                <Link inline href={`${import.meta.env.VITE_HOMEPAGE}/faq`}>FAQ</Link>
             </li>
             <li>
                 <Link inline href="https://www.buymeacoffee.com/idleberg" target="_blank">Buy me a coffee</Link>
