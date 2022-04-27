4566d1c960c09b168fec3574183448e13cb63929
Jan T. Sott <git@idleberg.com>
2022-04-27 22:09:49 +0200

    fix FAQ link

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -6,10 +6,10 @@
     <Column class="center" padding>
         <ul>
             <li>
-                <Link inline href="https://github.com/idleberg/playdate-arcade-fonts" target="_blank">Code on GitHub</Link>
+                <Link inline href="https://github.com/idleberg/playdate-arcade-fonts" target="_blank">GitHub</Link>
             </li>
             <li>
-                <Link inline href="/faq">FAQ</Link>
+                <Link inline href="${import.meta.env.VITE_HOMEPAGE}/faq">FAQ</Link>
             </li>
             <li>
                 <Link inline href="https://creativecommons.org/publicdomain/zero/1.0/" target="_blank">License</Link>
