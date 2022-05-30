d3e6ac3e0cb1f438fcfbf87770004b8bccf5255e
Jan T. Sott <git@idleberg.com>
2022-05-30 22:57:52 +0200

    add year tag

--- src/components/Tags.svelte
+++ src/components/Tags.svelte
@@ -9,9 +9,14 @@
     
     <Tag type="gray">{font.category}</Tag>
 
+    {#if (font.year.length)}
+        <Tag type="gray">{font.year.slice(0, 3)}0s</Tag>
+    {/if}
+
     {#if (font.features.missing.length)}
         <Tag type="red">incomplete</Tag>
     {/if}
+
     
     {#if (font.features.uppercase)}
         <Tag type="outline">uppercase</Tag>
