21d880d4728cf3745e9dc692d05cbabf91884550
Jan T. Sott <git@idleberg.com>
2022-07-19 00:52:04 +0200

    reset title

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -41,6 +41,10 @@
     }
 </script>
 
+<svelte:head>
+    <title>Playdate Arcade Fonts</title>
+</svelte:head>
+
 <Row>
     <Column padding lg={6} md={4} sm={8}>
         <Search placeholder={`Search ${publicFonts.length} fonts by name or category…`} bind:value={searchValue} on:input={changeHandler} on:clear={clearHandler} autofocus />
