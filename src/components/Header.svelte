3896ab9016da1fb3e1591af97a9f7ca3655b1edd
Jan T. Sott <git@idleberg.com>
2022-11-20 22:14:09 +0100

    style badge

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -7,7 +7,7 @@
         <a href={import.meta.env.VITE_HOMEPAGE || '/'}><h1>Playdate Arcade Fonts</h1></a>
         <p>A collection of bitmap fonts for <Link size="lg" href="https://play.date/">Playdate</Link>, the tiny handheld game system from Panic</p>
     </Column>
-    <Column padding style="text-align: right">
+    <Column padding class="badge">
         <a href="https://www.buymeacoffee.com/idleberg" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="height: 60px !important;width: 217px !important;" ></a>
     </Column>
 </Row>
@@ -16,5 +16,15 @@
     a {
         color: inherit;
         text-decoration: none;
+    }
+
+    :global(.badge) {
+        text-align: left;
+    }
+
+    @media screen and (min-width: 481px) {
+        :global(.badge) {
+            text-align: right;
+        }
     }
 </style>
