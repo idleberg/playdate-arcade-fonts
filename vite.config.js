61f887f77dc4a4fd1669240c739d546f26612496
Jan T. Sott <git@idleberg.com>
2023-10-11 21:19:34 +0200

    add vite-plugin-mkcert

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { resolve } from 'node:path';
 import { sveltekit } from '@sveltejs/kit/vite';
+import mkcert from 'vite-plugin-mkcert';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
+	plugins: [mkcert(), sveltekit()],
 	resolve: {
 		alias: {
 			$components: resolve('./src/components'),
